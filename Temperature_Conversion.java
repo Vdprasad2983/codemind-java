47a22bb76838b9671e30ff1dd2bebfa34347f371
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-07-17 21:45:36 +0530

    import java.util.*; public class triangle {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float c=sc.nextFloat();         float f=(32+(c*9/5));         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class triangle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float c=sc.nextFloat();
+        float f=(32+(c*9/5));
+        System.out.format("%.2f",f);
+    }
+}
