f428b028d2759d6551c2b9ec5524e96a39a1ffbb
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-07-17 21:35:57 +0530

    import java.util.*; public class circle {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         System.out.format("%.2f",(3.14*a*a));     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class circle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.format("%.2f",(3.14*a*a));
+    }
+}
